2d6fa315831483fa241bf398ce442c94cbd9bcba
Wu Jiang <masterwujiang@gmail.com>
2010-10-24 02:22:55 +0800

    clean code

--- app.rb
+++ app.rb
@@ -3,34 +3,39 @@ require 'sinatra'
 require 'yaml'
 require 'rdiscount'
 
+EXT = ".txt"
+
+class Article
+  attr_reader :title, :body
+
+  def initialize(file_path)
+    yaml, md = File.read(file_path).split("\n\n", 2)
+
+    @options = YAML.load(yaml)
+    @options["file_name"] = File.basename(file_path, EXT).split('-',4)[-1]
+
+    @body = RDiscount.new(md).to_html
+  end
+
+  def path
+    @options["date"].strftime("/%Y/%m/%d/")+ @options["file_name"]
+  end
+
+  def title
+    @options["title"]
+  end
+end
+
 get '/' do
   @articles = []
-  Dir.glob("articles/*.txt").each do |article_file|
-    @articles << File.read(article_file)
+  Dir.glob("articles/*#{EXT}").each do |article_file|
+    @articles << Article.new(article_file)
   end
-  @parsed = @articles.map do |article|
-    yaml, md = article.split("\n\n", 2)
-    hash = YAML.load(yaml)
-    md = RDiscount.new(md).to_html
-
-    [hash, md]
-  end
-
   haml :index
 end
 
 get '/:year/:month/:day/:title' do |year, month, day, title|
-  article_file = "articles/#{year}-#{month}-#{day}-#{title.gsub(" ","-").downcase}.txt"
-  puts article_file
-    article = File.read(article_file)
-    yaml, md = article.split("\n\n", 2)
-    hash = YAML.load(yaml)
-    md = RDiscount.new(md).to_html
-
-    @article = [hash.inspect, md]
-    haml :show
-end
-
-def to_url(hash)
-  "/#{Time.parse(hash["date"]).strftime("%Y/%m/%d")}/#{hash["title"].gsub(" ", "-")}"
+  article_file = "articles/#{year}-#{month}-#{day}-#{title.downcase}#{EXT}"
+  @article = Article.new(article_file)
+  haml :show
 end
