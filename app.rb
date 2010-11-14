03f5eb3ed146cd6371b25eeaf48405aa76b412cd
Wu Jiang <masterwujiang@gmail.com>
2010-11-14 21:03:53 +0800

    use configure to load all articles at start

--- app.rb
+++ app.rb
@@ -12,34 +12,38 @@ class Article
     yaml, md = File.read(file_path).split("\n\n", 2)
 
     @options = YAML.load(yaml)
-    @options["file_name"] = File.basename(file_path, EXT).split('-',4)[-1]
+    @options["slug"] = File.basename(file_path, EXT).split('-',4)[-1]
 
     @body = RDiscount.new(md).to_html
   end
 
   def path
-    @options["date"].strftime("/%Y/%m/%d/")+ @options["file_name"]
+    @options["date"].strftime("/%Y/%m/%d/")+ @options["slug"]
   end
 
-  def title
-    @options["title"]
+  %w(title date slug).each do |m|
+    class_eval "def #{m};@options['#{m}'];end"
   end
+end
 
-  def date
-    @options["date"]
+configure do
+  ALL_ARTICLES = Dir.glob("articles/*#{EXT}").map do |article_file|
+    Article.new(article_file)
   end
 end
 
 get '/' do
-  @articles = []
-  Dir.glob("articles/*#{EXT}").each do |article_file|
-    @articles << Article.new(article_file)
-  end
+  @articles = ALL_ARTICLES
   haml :index
 end
 
-get '/:year/:month/:day/:title' do |year, month, day, title|
-  article_file = "articles/#{year}-#{month}-#{day}-#{title.downcase}#{EXT}"
-  @article = Article.new(article_file)
+get '/:year/:month/:day/:slug' do |year, month, day, slug|
+  @article = ALL_ARTICLES.find do |article| 
+    article.date == Date.civil(year.to_i, month.to_i, day.to_i) &&
+      article.slug == slug
+  end
+
+  halt 404 unless @article
+                               
   haml :show
 end
